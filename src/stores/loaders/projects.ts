ceaaca00154369615a4e02fe02bf4c98e64a147e
Pothuraju <ram.k160796@gmail.com>
2024-12-20 11:57:30 +0000

    stale-while-revalidate for projects

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,18 +1,37 @@
-import type { Projects } from "@/utils/supaQueries"
-import { projectsQuery } from "@/utils/supaQueries"
-import { useMemoize } from "@vueuse/core"
+import { projectsQuery } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
+import type { Projects } from '@/utils/supaQueries'
 
 export const useProjectsStore = defineStore('projects-store', () => {
-    const projects = ref<Projects | null>(null)
-    const loadProjects = useMemoize(async (key: string) => await projectsQuery)
+  const projects = ref<Projects | null>(null)
+  const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
-    const getProjects = async () => {
-        const { data, error, status } = await loadProjects('projects')
-
-        if (error) useErrorStore().setError({ error, customCode: status })
-
-        projects.value = data
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          console.log('Cached and fresh data matched!')
+          return
+        } else {
+          console.log('Somehting has changed!')
+          loadProjects.delete('projects')
+        }
+      })
     }
+  }
 
-    return { projects, getProjects }
+  const getProjects = async () => {
+    const { data, error, status } = await loadProjects('projects')
+
+    if (error) useErrorStore().setError({ error, customCode: status })
+
+    projects.value = data
+
+    validateCache()
+  }
+
+  return {
+    projects,
+    getProjects
+  }
 })
