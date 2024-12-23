76a7868496f1bb75192053add422435bad2dddd0
Pothuraju <ram.k160796@gmail.com>
2024-12-23 05:56:21 +0000

    rendering layout based on user login

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 const errorStore = useErrorStore()
-const { activeError } = storeToRefs(useErrorStore())
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
@@ -9,18 +8,30 @@ onErrorCaptured((error) => {
 onMounted(() => {
   useAuthStore().trackAuthChanges()
 })
+
+const { user } = storeToRefs(useAuthStore())
+
+const AuthLayout = defineAsyncComponent(
+  () => import('./components/Layout/main/AuthLayout.vue')
+)
+
+const GuestLayout = defineAsyncComponent(
+  () => import('./components/Layout/main/GuestLayout.vue')
+)
 </script>
 
 <template>
-  <AuthLayout>
-    <AppErrorPage v-if="activeError" />
+  <Component :is="user ? AuthLayout : GuestLayout">
+    <AppErrorPage v-if="errorStore.activeError" />
+
     <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
-        <Component :is="Component" :key="route.name" />
+        <Component :is="Component" :key="route.name"></Component>
+
         <template #fallback>
-          <span>Loading ...</span>
+          <span>Loading ..</span>
         </template>
       </Suspense>
     </RouterView>
-  </AuthLayout>
+  </Component>
 </template>
